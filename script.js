21de4f956829e6859add963729038c1a46de3393
IxAMConst <160686957+IxAMConst@users.noreply.github.com>
2024-02-29 22:36:54 -0600

    update
    nav bar change update

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
 
-console.log('You have executed software developed by IxAM LLC, IxAM Const thank you for being apart of our journey')
-// Function to open a specific tab
-function openTab(evt, tabName) {
-    // Get all elements with class="tabcontent" and hide them
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    var tablinks = document.getElementsByClassName("tablinks");
-    for (var i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("active");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.classList.add("active");
-  }
-  console.log('Prepartory events Developed by IxAM Const have been execute');
- 
-  
-/*const letters = document.getElementById('changeMe');
-
-const boxOne = document.getElementsByName('boxChange');
-
-boxOne.addEventListener('click', () => {
-  if(box.style.background === 'green') {
-   box.style.background = 'blue'
-} else {
-   box.style.background = 'green'
- }
-});*/
-
-
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -56,4 +21,5 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   }
 });
+console.log('style changed execution, IxAM Const')
 
